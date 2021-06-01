59270ef924ef1e8517f751e3f5ef9508c4f75f0b
hongsokan_mac <hongsokan@daum.net>
2021-06-01 20:06:15 +0900

    client settings

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,14 +1,16 @@
-import { TableCell, TableRow } from '@material-ui/core'
 import React, { Component } from 'react'
+import { TableCell, TableRow } from '@material-ui/core'
 
 export default class Customer extends Component {
     render() {
         return (
             <TableRow>
                 <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell>{this.props.birthday}</TableCell>
-                <TableCell>{this.props.description}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
             </TableRow>
         )
     }
